f5a3f71ec6e471fb8e6af9b563d8a5248d2873ba
Joao Saavedra <monteir4@coventry.ac.uk>
2022-06-29 01:23:01 +0000

    updated home page content in html and module file

--- spa/js/home.js
+++ spa/js/home.js
@@ -11,28 +11,45 @@ export async function setup(node) {
 		customiseNavbar(['home', 'foo', 'logout']) // navbar if logged in
 		const token = localStorage.getItem('authorization')
 		console.log(token)
-		if(token === null) customiseNavbar(['home', 'register', 'login']) //navbar if logged out
+		if(token === null) {
+			customiseNavbar(['home', 'register', 'login']) //navbar if logged out
+			await addContent(node)
+		} else {
+			await showContent(node)
+		}
 		// add content to the page
-		await addContent(node)
+		//await addContent(node)
 	} catch(err) {
 		console.error(err)
 	}
 }
 
-// this example loads the data from a JSON file stored in the uploads directory
 async function addContent(node) {
 	// show "LOADING" message
 	document.querySelector('aside > p').innerText = 'LOADING'
 	document.querySelector('aside').classList.remove('hidden')
-	const response = await fetch('/uploads/quotes.json')
-	const quotes = await response.json()
-	const template = document.querySelector('template#quote')
-	for(const quote of quotes.data) {
-		const fragment = template.content.cloneNode(true)
-		fragment.querySelector('h2').innerText = quote.author
-		fragment.querySelector('p').innerText = quote.quote
-		node.appendChild(fragment)
-	}
+	const template = document.querySelector('template#loggedoff')
+	const fragment = template.content.cloneNode(true)
+	fragment.querySelector('h2').innerText = "Logged off page"
+	fragment.querySelector('p').innerText = "Loren ipsum"
+	node.appendChild(fragment)
+
+	// hide "LOADING" message
+	document.querySelector('aside').classList.add('hidden')
+}
+
+async function showContent(node) {
+	// show "LOADING" message
+	document.querySelector('aside > p').innerText = 'LOADING'
+	document.querySelector('aside').classList.remove('hidden')
+	const template = document.querySelector('template#loggedin')
+	const fragment = template.content.cloneNode(true)
+	fragment.querySelector('h2').innerText = "Logged in page"
+	fragment.querySelector('p').innerText = "Loren ipsum"
+	node.appendChild(fragment)
+
+	document.querySelector
+
 	// hide "LOADING" message
 	document.querySelector('aside').classList.add('hidden')
 }
